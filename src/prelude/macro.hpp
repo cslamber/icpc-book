4660f303d23d7fce182b67c40aafdc9f99fc0824
Chris Lambert <cslamber@andrew.cmu.edu>
2022-02-04 05:10:26 -0500

    .

--- src/prelude/macro.hpp
+++
@@ -1,2 +0,0 @@
-#define L1(...) [&](const auto& it) { return __VA_ARGS__; }
-#define L2(...) [&](const auto& it1, const auto& itr) { return __VA_ARGS__; }
--- src/util/flist.hpp
+++
@@ -1,7 +0,0 @@
-tcT struct flist{
-struct elem{T hd;shared_ptr<elem>tl;};
-using sp=shared_ptr<elem>;
-sp v;flist(){}flist(T hd,flist tl):v(new elem{hd,tl.v}){}
-flist operator>>(T hd)const{return flist(hd,*this);}
-v<T> to_v()const{v<T> r;for(sp c=v;c;c=c->tl)r.Pb(c->hd);return r;}
-};
